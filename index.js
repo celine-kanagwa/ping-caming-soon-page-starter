a5f69fc062c42b6944984e8092c420b689fcdb24
celine-kanagwa <celinekanagwa@gmail.com>
2021-08-11 22:46:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const emailInputElement = document.getElementById("email");
+const submitBtn = document.getElementById("submit");
+
+function isValidEmail(email){
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+submitBtn.addEventListener("click", function(e)
+{
+    if (!isValidEmail(emailInputElement.value))
+    {
+        e.preventDefault();
+        emailInputElement.style.border = "1px solid hsl(354, 100%, 66%)";
+        emailInputElement.nextElementSibling.textContent = "Please provide a valid email address";
+
+    }
+    else
+    {
+        emailInputElement.style.border = "1px solid hsl(223, 100%, 88%)";
+        emailInputElement.nextElementSibling.textContent = "please provide good email,adress ";    
+    }
+});
